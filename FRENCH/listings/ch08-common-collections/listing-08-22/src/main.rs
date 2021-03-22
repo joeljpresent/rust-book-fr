b24ac53fe617fc6927e635652cf293171463d75c
Joël Present <jpresent@opensense.fr>
2021-03-22 21:30:29 +0100

    08-03 listing08-22 slimmer

--- FRENCH/listings/ch08-common-collections/listing-08-22/src/main.rs
+++ FRENCH/listings/ch08-common-collections/listing-08-22/src/main.rs
@@ -7,8 +7,8 @@ fn main() {
 
     let mut table = HashMap::new();
     table.insert(nom_champ, valeur_champ);
-    // nom_champ et valeur_champ ne sont plus en vigueur à partir d'ici, essayez
-    // de les utiliser et vous verrez l'erreur du compilateur que vous
-    // obtiendrez !
+    // nom_champ et valeur_champ ne sont plus en vigueur à partir d'ici,
+    // essayez de les utiliser et vous verrez l'erreur du compilateur que
+    // vous obtiendrez !
     // ANCHOR_END: here
 }
